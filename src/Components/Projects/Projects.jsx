953b7c91f131232c1930150a13cc0744cfa017cd
raymondliu <liurui940802@hotmail.com>
2022-09-17 01:17:53 -0400

    update all projects

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -11,32 +11,40 @@ export default function (props) {
 
             <ProjectCard
                 title="Stay-Put"
-                description=""
-                tech=""
+                description="Stay / Put lets renters and buyers quickly connect with realtors to discuss properties for rent/sale! Create a profile, browse as a user, or become an agent and list properties for other users to see."
+                tech="Python, Django, HTML5, CSS3, JavaScript, Bootstrap5, PostgreSQL, AWS S3"
                 login=""
                 githubLink="https://github.com/Reidwids/Stay-Put"
-                link=""
+                link="https://stay-put.herokuapp.com/"
+            />
+            <ProjectCard
+                title="Ray's Portfolio"
+                description="This is my portfolio, to introduce myself and show my previous projects. This project is hosted on Heroku."
+                tech="React, JavaScript, CSS3, HTML5"
+                login=""
+                githubLink="https://github.com/raymondliuca/developer-portfolio"
+                link="https://raysportfolio.herokuapp.com/"
             />
             <ProjectCard
                 title="PostBar"
-                description=""
-                tech=""
+                description="PostBar is a Reddit-Like app for people to discuss technical problems and free chat. This project is hosted on Heroku."
+                tech="React, Node.js, Express, MongoDB, AJAX, JWT authentication, HTML5, CSS3"
                 login=""
                 githubLink="https://github.com/raymondliuca/PostBar-Backend"
-                link="https://postbar.herokuapp.com/"
+                link=""
             />
             <ProjectCard
                 title="Knife-Collection"
-                description=""
-                tech=""
+                description="Knife-Collection is a web app build by a kitchen knife lover, aim to be a knife-pedia, full CRUD app hosted on Heroku."
+                tech="HTML5, CSS3, JavaScript, Express, EJS, MongoDB"
                 login=""
                 githubLink="https://github.com/raymondliuca/Knife-Collection"
                 link="https://raysknifecollection.herokuapp.com/"
             />
             <ProjectCard
                 title="BlackJack"
-                description=""
-                tech=""
+                description="Famous casino game online, try to get bigger than the dealer or go stright forward to 21! But don't forget to avoid busting! This project is hosted by Github."
+                tech="JavaScript, HTML5, CSS3"
                 login=""
                 githubLink="https://github.com/raymondliuca/BlackJack"
                 link="https://raymondliuca.github.io/BlackJack/"
